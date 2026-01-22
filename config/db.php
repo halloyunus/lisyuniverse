3712ebddd63a4633478a8ca1232f6db771c71618
Muhamad Yunus <90395805+halloyunus@users.noreply.github.com>
2026-01-22 21:43:17 +0700

    Update db.php
    update del user pass db

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "lisyuniv_lisyuniverse";
-$pass = "lisyuniverse20260326";
-$db   = "lisyuniv_undangandigital";
+$user = "";
+$pass = "";
+$db   = "";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
